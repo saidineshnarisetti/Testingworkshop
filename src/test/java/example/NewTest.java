a54b2e96fee52cece204b5281ce856a2d7d93264
saidineshnarisetti <dinu.narisetti@gmail.com>
2020-04-20 16:20:02 +0530

    ’Test_message’

---
+++ src/test/java/example/NewTest.java
@@ -0,0 +1,33 @@
+package example;
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+	private WebDriver driver;
+  @Test
+  public void testEasy() {	
+	  
+		driver.get("http://demo.guru99.com/test/guru99home/");  
+		String title = driver.getTitle();				 
+		AssertJUnit.assertTrue(title.contains("Demo Guru99 Page")); 		
+	}	
+  @BeforeTest
+  public void beforeTest() {
+	  System.setProperty("webdriver.chrome.driver","src/drivers/chromedriver");
+		driver = new ChromeDriver();
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  driver.quit();	
+  }
+
+}
